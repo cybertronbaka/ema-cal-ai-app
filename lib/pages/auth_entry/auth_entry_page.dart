a257f71f6a43a774106b05579b9c6012c574d5ae
Dorji Gyeltshen <dg.dorjigyeltshen@gmail.com>
2025-04-09 02:18:55 +0600

    refactor: Auth Entry Page to use HookWidget instead of HookConsumerWidget

--- lib/pages/auth_entry/auth_entry_page.dart
+++ lib/pages/auth_entry/auth_entry_page.dart
@@ -3,17 +3,16 @@ library;
 import 'package:ema_cal_ai/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 part 'enums/onboarding_step.dart';
 part 'widgets/image_section.dart';
 part 'widgets/indicators.dart';
 
-class AuthEntryPage extends HookConsumerWidget {
+class AuthEntryPage extends HookWidget {
   const AuthEntryPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final textTheme = TextTheme.of(context);
     final isAttached = useValueNotifier(false);
     final pageController = usePageController(
