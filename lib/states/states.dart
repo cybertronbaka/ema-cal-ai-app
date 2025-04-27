27678067d422434a283c429c3144cfc35ffc8c02
Dorji Gyeltshen <dg.dorjigyeltshen@gmail.com>
2025-04-27 23:36:22 +0600

    chore: Fix Analysis warnings

--- lib/states/states.dart
+++ lib/states/states.dart
@@ -7,7 +7,6 @@ import 'package:ema_cal_ai/models/nutrition_plan.dart';
 import 'package:ema_cal_ai/models/onboarding_data.dart';
 import 'package:ema_cal_ai/models/user_profile.dart';
 import 'package:ema_cal_ai/repos/history_repo/history_repo.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info_plus/package_info_plus.dart';
